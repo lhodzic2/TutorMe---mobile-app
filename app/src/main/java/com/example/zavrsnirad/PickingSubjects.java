08abdfe37d0f8676da332634ba3f2ff8334e27ac
lhodzic2 <lhodzic2@etf.unsa.ba>
2021-07-27 23:54:20 +0200

    Sitne ispravke

---
+++ app/src/main/java/com/example/zavrsnirad/PickingSubjects.java
@@ -0,0 +1,14 @@
+package com.example.zavrsnirad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PickingSubjects extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picking_subjects);
+    }
+}
