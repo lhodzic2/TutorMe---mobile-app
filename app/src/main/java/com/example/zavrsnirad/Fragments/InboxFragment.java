e525106557dfb78418d649fa8d1b9a6c1567ffc2
lhodzic2 <lhodzic2@etf.unsa.ba>
2021-08-12 15:33:28 +0200

    Napravljen inbox fragment

--- app/src/main/java/com/example/zavrsnirad/Fragments/InboxFragment.java
+++ app/src/main/java/com/example/zavrsnirad/Fragments/InboxFragment.java
@@ -2,65 +2,112 @@ package com.example.zavrsnirad.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
+import com.example.zavrsnirad.Adapter.UserAdapter;
 import com.example.zavrsnirad.R;
+import com.example.zavrsnirad.model.MessageList;
+import com.example.zavrsnirad.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link InboxFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InboxFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private UserAdapter userAdapter;
+    private List<User> users;
+    private FirebaseUser firebaseUser;
+    private DatabaseReference db;
+    private List<MessageList> messages;
+    private RecyclerView recycler;
 
     public InboxFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InboxFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static InboxFragment newInstance(String param1, String param2) {
-        InboxFragment fragment = new InboxFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inbox, container, false);
+        View view = inflater.inflate(R.layout.fragment_inbox, container, false);
+        recycler = view.findViewById(R.id.recyclerInbox);
+        recycler.setHasFixedSize(true);
+        recycler.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        messages = new ArrayList<>();
+        db = FirebaseDatabase.getInstance("https://zavrsni-rad-200d4-default-rtdb.europe-west1.firebasedatabase.app/").getReference("MessageList").child(userID);
+
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                messages.clear();
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    MessageList messageList = ds.getValue(MessageList.class);
+                    messages.add(messageList);
+                }
+                getAllMessages();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+        return view;
+    }
+
+    private void getAllMessages() {
+        users = new ArrayList<>();
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseFirestore.collection("users").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable @org.jetbrains.annotations.Nullable QuerySnapshot value, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
+                users.clear();
+
+                for (DocumentChange documentChange : value.getDocumentChanges()) {
+                    User user = documentChange.getDocument().toObject(User.class);
+
+                    for (MessageList msg : messages) {
+                        if (user.getId().equals(msg.getId())) {
+                            users.add(user);
+                        }
+                    }
+                }
+
+                userAdapter = new UserAdapter(getContext(),users);
+                recycler.setAdapter(userAdapter);
+                userAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
